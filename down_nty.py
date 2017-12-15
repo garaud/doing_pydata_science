c2a54aa56141ec7bf8c49d9218275041498c04af
Damien Garaud <damien.garaud@oslandia.com>
2017-12-15 20:56:53 +0100

    update the script which downloads nyt CSV files

--- down_nty.py
+++ down_nty.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 
 """Download nyt*.csv files needed in Chapter 2 - Exploratory Data Analysis
+
+Python 3
 """
 
-from __future__ import print_function
 import os
 import requests
+
 
 URL = "http://stat.columbia.edu/~rachel/datasets"
 BASENAME = "nyt{}.csv"
 BUFSIZE = 1024
 OUTPUT_DIR = "nyt-data"
+
 
 def download_single_file(number):
     fname = BASENAME.format(number)
@@ -18,12 +21,12 @@ def download_single_file(number):
     resp = requests.get(url, stream=True)
     with open(os.path.join(OUTPUT_DIR, fname), 'w') as fobj:
         for chunk in resp.iter_content(BUFSIZE):
-            fobj.write(chunk)
+            fobj.write(str(chunk))
 
 
 if __name__ == '__main__':
     if not os.path.isdir(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
-    for n in xrange(1, 32):
+    for n in range(1, 32):
         print("Downloading... file " + BASENAME.format(n))
         download_single_file(n)
